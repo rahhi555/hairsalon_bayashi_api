f60a87604e9445aff6509cdea42095365aeb2353
rahhi555 <whowhose0904@gmail.com>
2021-04-16 00:16:22 +0900

    production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'http://localhost:8080',
+            'http://localhost:80',
+            'https://localhost:80',
+            'http://localhost',
+            ENV["FRONT_URL"]
 
     resource '*',
              headers: :any,
--- db/migrate/20210316032959_add_uid_to_stylists.rb
+++ db/migrate/20210316032959_add_uid_to_stylists.rb
@@ -1,5 +1,5 @@
 class AddUidToStylists < ActiveRecord::Migration[6.1]
   def change
-    add_column :stylists, :uid, :string, null: false
+    add_column :customers, :uid, :string, null: false
   end
 end
