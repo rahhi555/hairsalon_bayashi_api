1c5e9fbcf42ad7ed8c74505e47a52fb3067a7c95
rahhi555 <whowhose0904@gmail.com>
2021-08-15 20:25:55 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -e
-
-echo FRONT_URL: $FRONT_URL
-echo RAILS_DB_HOST: $RAILS_DB_HOST
-echo APP_DATABASE_PASSWORD: $APP_DATABASE_PASSWORD
-echo RAILS_MASTER_KEY $RAILS_MASTER_KEY
-
-cd $APP_ROOT
-
-rm -f $APP_ROOT/tmp/pids/server.pid
-rails db:create RAILS_ENV=production
-rails db:migrate RAILS_ENV=production
-rails server -b 0.0.0.0 -p 3000 -e production
+#set -e
+#
+#echo FRONT_URL: $FRONT_URL
+#echo RAILS_DB_HOST: $RAILS_DB_HOST
+#echo APP_DATABASE_PASSWORD: $APP_DATABASE_PASSWORD
+#echo RAILS_MASTER_KEY $RAILS_MASTER_KEY
+#
+#cd $APP_ROOT
+#
+#rm -f $APP_ROOT/tmp/pids/server.pid
+#rails db:create RAILS_ENV=production
+#rails db:migrate RAILS_ENV=production
+#rails server -b 0.0.0.0 -p 3000 -e production
